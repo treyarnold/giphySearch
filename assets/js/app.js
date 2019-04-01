d794cd0163eba35099ffb0b7dfd4b7121d19e3d9
Trey Arnold <arnold.trey80@gmail.com>
2019-04-01 10:05:57 -0500

    gif clicking done

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,9 +7,13 @@ const app = {
     // let buttonDiv = "#buttons";
     $("#buttons").empty();
     this.buttons.forEach(element => {
-      let newButton = `<button class="btn btn-success m-2">${element}</button>`;
+      let newButton = `<button class="btn btn-info m-2 gifSearchButton">${element}</button>`;
       $("#buttons").append(newButton);
     })
+  },
+
+  gifAnimateHandler: function (event) {
+    console.log(this);
   },
 
   showGiphy: function (searchName) {
@@ -26,7 +30,9 @@ const app = {
           var gifDiv = 
             `<div class="col-md-4">
               <p>Rating: ${results[i].rating}</p>
-              <img src="${results[i].images.fixed_height.url}"
+              <img class="gif" src="${results[i].images.fixed_height_still.url}" 
+                dataStill="${results[i].images.fixed_height_still.url}"
+                dataAnimate="${results[i].images.fixed_height.url}" state="still"
             </div>`
           $("#gifDiv").prepend(gifDiv);
           if ((i + 1) % 3 === 0) {
@@ -38,5 +44,25 @@ const app = {
   }
 }
 
-$(document).on("click", "button", event => {app.showGiphy(event.target.textContent)})
-app.showButtons();
+$(document).on("click", ".gifSearchButton", event => {app.showGiphy(event.target.textContent)});
+$(document).on("click", ".gif", function() {
+  console.log("gif clicked");
+  let state = $(this).attr("state");
+  if (state === "still") {
+    let src = $(this).attr("dataAnimate");
+    $(this).attr("src", src);
+    $(this).attr("state", "animate");
+  } else {
+    let src = $(this).attr("dataStill");
+    $(this).attr("src", src);
+    $(this).attr("state", "still");
+  }
+});
+$(document).ready(function() {
+  app.showButtons();
+  $("#newButton").on("click", event => {
+    event.preventDefault();
+    app.buttons.push($("#buttonInput").val());
+    app.showButtons();
+  })
+})
